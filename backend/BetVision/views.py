9caf375707904775f034564ff3960c22cddee263
VirajCommits <virajmurab567@yahoo.com>
2024-11-14 20:52:50 -0700

    setup

--- backend/BetVision/views.py
+++ backend/BetVision/views.py
@@ -1,9 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 
 # Create your views here.
 
-class HelloWorld(APIView):
-    def get(self, request):
-        return Response({"message": "Hello, World!"})
+
+@api_view(['GET'])
+def home(request):
+    return JsonResponse({"message": "Welcome to the home page!"})  # Correct return type
